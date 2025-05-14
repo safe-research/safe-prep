fe03b0b203cbc2de623bb3a4af6eaea70824a067
Nicholas Rodrigues Lordello <nick@safe.global>
2025-05-14 17:38:37 +0200

    Contract Documentation

--- src/SafePREP.sol
+++ src/SafePREP.sol
@@ -80,6 +80,13 @@ contract SafePREP {
         _fallback(_implementation);
     }
 
+    /// @notice Setup a Safe PREP account.
+    /// @dev This can only be called while the account is uninitialized. Calling
+    ///      it after the account has been initialized will forward the call to
+    ///      the account implementation.
+    /// @param implementation The {Safe} implementation.
+    /// @param setup The {Safe.setup} parameters.
+    /// @param salt The PREP account salt.
     function setup7702(address implementation, Setup calldata setup, uint256 salt) external onlyUninitialized {
         (bytes32 initHash, bytes memory init) = _init(implementation, setup);
         (address account,) = _prep(initHash, salt);
@@ -97,6 +104,24 @@ contract SafePREP {
         emit ProxyCreation(address(this), implementation);
     }
 
+    /// @notice Generate parameters for a Safe PREP account, and compute a valid
+    ///         `salt` for an authorization.
+    /// @dev This function accepts a `startingSalt` and returns an actual `salt`
+    ///      to use, since not all `r` and `s` values can be recovered to an
+    ///      address. This function searches for the first `salt` value starting
+    ///      from `startingSalt` that produces a valid signature.
+    ///      This can only be called while the {SafePREP} contract directly.
+    ///      Calling it on a Safe PREP account will cause the call to be
+    ///      forwarded to its implementation.
+    /// @param implementation The {Safe} implementation.
+    /// @param setup The {Safe.setup} parameters.
+    /// @param startingSalt The PREP account salt.
+    /// @return account The address of the generated Safe PREP account.
+    /// @return salt The salt for the account.
+    /// @return yParity The y-parity value for the EIP-7702 authorization
+    ///                 signature.
+    /// @return r The EIP-7702 authorization signature R-value.
+    /// @return s The EIP-7702 authorization signature S-value.
     function generateAccount(address implementation, Setup calldata setup, uint256 startingSalt)
         external
         onlySelf
@@ -106,12 +131,16 @@ contract SafePREP {
         salt = startingSalt;
         (account, r) = _prep(initHash, salt);
         while (account == address(0)) {
-            salt++;
+            unchecked {
+                salt++;
+            }
             (account, r) = _prep(initHash, salt);
         }
         return (account, salt, _Y_PARITY, r, _S);
     }
 
+    /// @notice Internal fallback implementation.
+    /// @param implementation The implementation address to delegate to.
     function _fallback(address implementation) private {
         assembly ("memory-safe") {
             let ptr := mload(0x40)
@@ -124,7 +153,38 @@ contract SafePREP {
         }
     }
 
-    function _authorization() internal view returns (bytes32 authorization) {
+    /// @notice Compute the {Safe} initialization data and hash.
+    function _init(address implementation, Setup memory setup)
+        internal
+        pure
+        returns (bytes32 initHash, bytes memory init)
+    {
+        init = abi.encodeWithSignature(
+            "setup(address[],uint256,address,bytes,address,address,uint256,address)",
+            setup.owners,
+            setup.threshold,
+            setup.initializer,
+            setup.initializerData,
+            setup.fallbackHandler,
+            address(0),
+            0,
+            payable(address(0))
+        );
+        initHash = keccak256(abi.encode(implementation, keccak256(init)));
+        return (initHash, init);
+    }
+
+    /// @notice Computes the
+    function _prep(bytes32 initHash, uint256 salt) internal view returns (address account, bytes32 r) {
+        r = keccak256(abi.encode(initHash, salt));
+        account = ecrecover(_AUTHORIZATION, _V, r, _S);
+        return (account, r);
+    }
+
+    /// @notice Compute the EIP-7702 authorization hash for delegating to the
+    ///         current contract.
+    /// @dev This function should only be called in the constructor.
+    function _authorization() private view returns (bytes32 authorization) {
         uint8 addressLength = 20;
         bytes20 addressBits = bytes20(address(this));
         while (bytes1(addressBits) == 0) {
@@ -147,31 +207,5 @@ contract SafePREP {
                 uint8(0x80) // nonce = 0
             )
         );
-    }
-
-    function _init(address implementation, Setup memory setup)
-        internal
-        pure
-        returns (bytes32 initHash, bytes memory init)
-    {
-        init = abi.encodeWithSignature(
-            "setup(address[],uint256,address,bytes,address,address,uint256,address)",
-            setup.owners,
-            setup.threshold,
-            setup.initializer,
-            setup.initializerData,
-            setup.fallbackHandler,
-            address(0),
-            0,
-            payable(address(0))
-        );
-        initHash = keccak256(abi.encode(implementation, keccak256(init)));
-        return (initHash, init);
-    }
-
-    function _prep(bytes32 initHash, uint256 salt) internal view returns (address account, bytes32 r) {
-        r = keccak256(abi.encode(initHash, salt));
-        account = ecrecover(_AUTHORIZATION, _V, r, _S);
-        return (account, r);
     }
 }
